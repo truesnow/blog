2f53b394febd0e2a48613a217674cb9910a2cf82
truesnow <chrisx0909@gmail.com>
2018-05-06 13:09:09 +0000

    Finish home page UI

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="truesnow">
+    <meta name="description" content="Sleep, eat, code. TRUESNOW's personal website.">
+    <title>truesnow - SLEEP EAT CODE</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.gif">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="home-page">
+        <div class="container-fluid">
+            <!-- INTRODUCTION -->
+            <div class="row introduction">
+                <h1 class="name text-center">TRUE SNOW</h1>
+                <h2 class="role text-center">WEB DEVELOPER</h2>
+            </div>
+
+            <!-- MENU -->
+            <div class="row menu text-center">
+                <ul class="">
+                    <li>
+                        <a href="https://github.com/truesnow" title="truesnow" target="_blank">Github</a>
+                    </li>
+                    <li>
+                        <a href="/images/truesnow-wechat-qrcode.png" target="_blank" title="微信号：541817418" class="wechat-qrcode">Wechat</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- ABOUT -->
+            <div class="row about">
+                <div class="col-md-6 col-md-offset-3">
+                    <h2 class="text-center title">ABOUT</h2>
+                    <ul>
+                        <li>Experienced PHP developer - PHP / Laravel / Yii2</li>
+                        <li>Front end developer - HTML / CSS / jQuery / Bootstrap</li>
+                        <li>Other skills include - Node.js / Docker / Linux etc</li>
+                        <li>A life-long learner</li>
+                        <li>Passion in code</li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- PROJECTS -->
+            <div class="row projects">
+                <h2 class="text-center title">PERSONAL PROJECTS</h2>
+                <div class="col-md-10 col-md-offset-1 text-center">
+                    <ul>
+                        <li>
+                            <a href="#" title="Blog">
+                                <div class="image">
+                                    <img src="/images/blog.jpg" alt="BLOG">
+                                </div>
+                                <div class="detail text-center"> The blog that record the problems when coding and some stduy notes. </div> </a>
+                        </li>
+                        <li>
+                            <a href="#" title="Bookmark">
+                                <div class="image">
+                                    <img src="/images/bookmark.jpg" alt="BOOKMARK">
+                                </div>
+                                <div class="detail"> If you are also a developer, you may find some useful website in this website collecting site. </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" title="Book">
+                                <div class="image">
+                                    <img src="/images/books.jpg" alt="BOOK">
+                                </div>
+                                <div class="detail">The books which translate by me.</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" title="Pratice">
+                                <div class="image">
+                                    <img src="/images/pratice.jpg" alt="PRATICE">
+                                </div>
+                                <div class="detail">Some pratice jobs.</div>
+                            </a>
+                        </li>
+                    </ul>
+                    <br>
+                    <p class="text-cener"><a href="https://github.com/truesnow" target="_blank">View all of my projects on Github</a></p>
+                </div>
+            </div>
+
+            <!-- CONTACT -->
+            <div class="row contact">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2 class="text-center title">CONTACT</h2>
+                    <form action="">
+                        <div class="row info">
+                            <div class="col-md-6">
+                                <input type="text" name="name" id="name" placeholder="name" required>
+                            </div>
+                            <div class="col-md-6">
+                                <input type="email" name="email" id="email" placeholder="email" required>
+                            </div>
+                        </div>
+                        <div class="row content">
+                            <div class="col-md-12">
+                                <textarea name="content" id="content" rows="2" required></textarea>
+                            </div>
+                        </div>
+                        <div class="button">
+                            <button type="submit">Contact Me</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row footer">
+                <p class="text-center">Copyright &copy; 2018 truesnow</p>
+            </div>
+        </div>
+    </div>
+    <script src="/js/app.js"></script>
+</body>
+</html>
