ce91edca9e1e0812a87f652f087f64bb7b3f78cd
truesnow <chrisx0909@gmail.com>
2018-05-29 14:45:03 +0000

    create subjects

---
+++ database/migrations/2018_05_29_223546_create_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->comment('专题名称');
+            $table->string('description')->comment('专题描述');
+            $table->integer('article_count')->comment('专题下文章数量');
+            $table->integer('pid')->nullable(false)->default(0)->comment('父级专题ID，为0则为顶级');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
