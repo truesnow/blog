ff9780a69f4a68d9203a24702d495bf25a667a11
truesnow <chrisx0909@gmail.com>
2018-06-22 16:06:49 +0000

    modify mottos migrate

--- database/migrations/2018_06_10_174633_create_mottos_table.php
+++ database/migrations/2018_06_10_174633_create_mottos_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMottoesTable extends Migration
+class CreateMottosTable extends Migration
 {
 	public function up()
 	{
