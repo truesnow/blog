1ec4992bd868a08b472608bfc95bf46925654d0b
truesnow <chrisx0909@gmail.com>
2020-07-05 19:36:01 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 whoami
 git pull
 /usr/local/bin/composer install --prefer-dist --optimize-autoloader -vvv
-php artisan migrate
 php artisan vendor:publish --force
 php artisan view:clear
+php artisan migrate
+php artisan key:generate
